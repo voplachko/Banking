bdf7519a0ea4c2a2bdce4da79544437095d36729
voplachko <oplachko2003@gmail.com>
2024-07-19 14:47:45 +0500

    19 07 2024 2

--- Banking/Network/Workers/AuthWorker.swift
+++ Banking/Network/Workers/AuthWorker.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+final class AuthWorker {
+    func authMobPost(credentials: Credentials,
+                  failure: @escaping ((Any) -> Void),
+                  success: @escaping Success<Any>) {
+        
+        let parameters = ["login": credentials.login,
+                          "password": credentials.password]
+        
+        NetworkManager.shared.fetchData(from: "/auth/mobile", parameters: parameters, method: .post) { result in
+            
+            switch result {
+            case .success(let data):
+                // обработать данные
+                success(data)
+                
+            case .failure(let error):
+                
+                // Обработать ошибку
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func authMobGet(credentials: Credentials,
+                  failure: @escaping ((Any) -> Void),
+                  success: @escaping Success<Any>) {
+        
+        let parameters = ["login": credentials.login,
+                          "password": credentials.password]
+        
+        NetworkManager.shared.fetchData(from: "/auth/mobile", parameters: parameters, method: .get) { result in
+            
+            switch result {
+            case .success(let data):
+                // обработать данные
+                success(data)
+                
+            case .failure(let error):
+                
+                // Обработать ошибку
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+}
+
+
--- Banking/Source/Modules/Authorization/Registration/RegistrationViewModel.swift
+++ Banking/Source/Modules/Authorization/Registration/RegistrationViewModel.swift
@@ -10,6 +10,7 @@ import SwiftUI
 public struct Credentials {
     let login: String
     let email: String
+    let password: String
 }
 
 class RegistrationViewModel: ObservableObject {
@@ -33,10 +34,13 @@ extension RegistrationViewModel {
 extension RegistrationViewModel {
     
     func registerUser() {
-        registrationWorker.createAccount(credentials: Credentials(login: userNumber, email: userEmail),
+        registrationWorker.createAccount(credentials: Credentials(login: userNumber, email: userEmail, password: userPassword),
                                     failure: { error in
          // занимаемся обработкой ошибки
-            print((error as? NSError).localizedDescription)
+            if let error = error as? NSError {
+                print(error.localizedDescription)
+            }
+            self.showAlert = true
         },
                                     success: { result in
             // отправляем запрос на activateAccount
@@ -47,10 +51,12 @@ extension RegistrationViewModel {
     // Реализовать функцию activateAccount по аналогии c registerUser
     func activateAccount() {
         // при успехе в блоке success отправить запрос на setPassword
-        registrationWorker.activateAccount(credentials: Credentials(login: userNumber, email: userEmail),
+        registrationWorker.activateAccount(credentials: Credentials(login: userNumber, email: userEmail, password: userPassword),
                                     failure: { error in
-            print((error as? NSError).localizedDescription)
-         // занимаемся обработкой ошибки
+            if let error = error as? NSError {
+                print(error.localizedDescription)
+            }         // занимаемся обработкой ошибки
+            self.showAlert = true
         },
                                     success: { result in
             
@@ -64,14 +70,16 @@ extension RegistrationViewModel {
         // сохраняем данные если необходимо
         // показываем другой экран
         // сценарий после успешной регистрации
-        registrationWorker.setPassword(credentials: Credentials(login: userNumber, email: userEmail),
+        registrationWorker.setPassword(credentials: Credentials(login: userNumber, email: userEmail, password: userPassword),
                                     failure: { error in
-            print((error as? NSError).localizedDescription)
+            if let error = error as? NSError {
+                print(error.localizedDescription)
+            }
+            self.showAlert = true
          // занимаемся обработкой ошибки
         },
                                     success: { result in
             
-            self.setPassword()
         })
     }
 }
